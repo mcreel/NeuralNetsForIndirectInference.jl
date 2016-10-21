e69cbbd6320bdece4bc2262e16df57c14f0b80d3
Michael Creel <michael.creel@uab.es>
2016-10-21 17:26:50 +0200

    finish simplifying dsge

--- DSGE/300_40.jl
+++ DSGE/300_40.jl
@@ -1,19 +1,18 @@
 #=
-The training script for the 100-20 node MLP for the MA2 model
+This script trains and saves the 300-40 MLP for the DSGE example, computes
+RMSE for the Monte Carlo data, and creates the plot
 =#
 using MXNet,PyPlot
-include("dataprep.jl") # creates X,Y,XT,YT, the training and testing inputs and output
-
-# how to set up data providers using data in memory
-batchsize = 2048 # can adjust this later, but must be defined now for next line
-trainprovider = mx.ArrayDataProvider(:data => X, batch_size=batchsize, shuffle=false, :label => Y)
-evalprovider = mx.ArrayDataProvider(:data => XT, batch_size=batchsize, shuffle=false, :label => YT)
 
 # size of layers
 layer1 = 300
 layer2 = 40
 outputs = 9
-
+include("dataprep.jl") # creates X,Y,XT,YT, the training and testing inputs and output
+# how to set up data providers using data in memory
+batchsize = 2048 # can adjust this later, but must be defined now for next line
+trainprovider = mx.ArrayDataProvider(:data => X, batch_size=batchsize, shuffle=false, :label => Y)
+evalprovider = mx.ArrayDataProvider(:data => XT, batch_size=batchsize, shuffle=false, :label => YT)
 # set up 2 layer MLP with 2 outputs
 data = mx.Variable(:data)
 label = mx.Variable(:label)
@@ -22,43 +21,36 @@ net  = @mx.chain    mx.FullyConnected(data = data, num_hidden=layer1) =>
                     mx.FullyConnected(num_hidden=layer2) =>
                     mx.Activation(act_type=:tanh) =>
                     mx.FullyConnected(num_hidden=outputs)        
-
 # squared error loss is appropriate for regression, don't change
 cost = mx.LinearRegressionOutput(data = net, label=label)
-
 # final model definition, don't change, except if using gpu
 model = mx.FeedForward(cost, context=mx.cpu())
-
 # set up the optimizer: select one, explore parameters, if desired
-#optimizer = mx.SGD(lr=0.01, momentum=0.9, weight_decay=0.00001)
+# optimizer = mx.SGD(lr=0.01, momentum=0.9, weight_decay=0.00001)
 optimizer = mx.ADAM()
-
 # train, reporting loss for training and evaluation sets
 # initial training with small batch size, to get to a good neighborhood
-batchsize = 128
-mx.fit(model, optimizer, initializer=mx.NormalInitializer(0.0,0.1), eval_metric=mx.MSE(), trainprovider, eval_data=evalprovider, n_epoch = 100)
-# more training with larger sample
+batchsize = 256
+mx.fit(model, optimizer, initializer=mx.NormalInitializer(0.0,0.01), eval_metric=mx.MSE(), trainprovider, eval_data=evalprovider, n_epoch = 200)
+# more training with larger sample, saving the final fitted model
 batchsize = 2048
-mx.fit(model, optimizer, eval_metric=mx.MSE(), trainprovider, eval_data=evalprovider, n_epoch = 10)
-
+mx.fit(model, optimizer, eval_metric=mx.MSE(), trainprovider, eval_data=evalprovider, n_epoch = 20, callbacks=[mx.do_checkpoint("dsge30040", frequency=20, save_epoch_0=false)])
 # obtain predictions
 plotprovider = mx.ArrayDataProvider(:data => XMC, :label => YMC)
 fit = mx.predict(model, plotprovider)
 fit = fit'
-fit = preprocess + sErrors.*fit # back to original location and scale
-# keep the fits inside support
-fit[:,6] = fit[:,6].*(fit[:,6].> 0.0)
-fit[:,8] = fit[:,8].*(fit[:,8].> 0.0)
+fit = PostProcess(XMC', fit)
 # compute RMSE
-error = YMC' - fit
+error = Y_orig - fit
 bias = mean(error,1)
 mse = mean(error.^2,1)
 rmse = sqrt(mse)
-@printf("    bias      rmse       mse\n")
+relbias = 100*bias ./ mean(YMC',1) # relative measures, in percentage
+relrmse = 100*rmse ./ mean(YMC',1)
+@printf("    bias      rmse       mse   relbias   relrmse\n")
 for i=1:size(bias,2)
-    @printf("%8.5f  %8.5f  %8.5f\n", bias[i], rmse[i], mse[i])
+    @printf("%8.5f  %8.5f  %8.5f  %8.5f  %8.5f\n", bias[i], rmse[i], mse[i], relbias[i], relrmse[i])
 end
-
 # get the first layer parameters for influence analysis
 beta = copy(model.arg_params[:fullyconnected0_weight])
 z = maximum(abs(beta),2);
--- DSGE/dataprep.jl
+++ DSGE/dataprep.jl
@@ -1,33 +1,56 @@
 using JLD
+function CreatePreprocessItems()
+    traintest = load("dsge.jld", "traintest")
+    thetas = traintest[:,1:9]
+    Zs = traintest[:,10:end]
+    mZs = mean(Zs,1)
+    sZs = std(Zs,1)
+    Zs = (Zs .- mZs) ./ sZs
+    # get least squares fit
+    x = [ones(size(Zs,1)) Zs]
+    beta = x\thetas
+    # least squares errors
+    errors = thetas - x*beta
+    # for standardization
+    sErrors = std(errors,1)
+    return mZs, sZs, beta, sErrors
+end
+
+function Preprocess(data)
+    mZs, sZs, beta, sErrors = CreatePreprocessItems()
+    Y_orig = data[:,1:9]
+    Zs = data[:,10:end]
+    Zs = (Zs .- mZs) ./ sZs
+    x = [ones(size(Zs,1)) Zs]
+    # least squares errors
+    errors = Y_orig - x*beta
+    # for standardization
+    sErrors = std(errors,1)
+    errors = errors ./ sErrors
+    Y = errors'
+    X = Zs'
+    return Y,X, Y_orig 
+end    
+
+# this function takes the net's inputs and outputs
+# and generates the fit for the original unprocessed
+# outputs
+function PostProcess(inputs, outputs)
+    mZs, sZs, beta, sErrors = CreatePreprocessItems()
+    x = [ones(size(inputs,1)) inputs]
+    fit = x*beta + sErrors.*outputs # back to original location and scale
+    # keep the fits inside support
+    fit[:,6] = fit[:,6].*(fit[:,6].> 0.0)
+    fit[:,8] = fit[:,8].*(fit[:,8].> 0.0)
+    return fit
+end    
 traintest = load("dsge.jld", "traintest")
-thetas = traintest[:,1:9]
-Zs = traintest[:,10:end]
+Y,X = Preprocess(traintest)
 trainsize = 500000
-mZs = mean(Zs,1)
-sZs = std(Zs,1)
-Zs = (Zs .- mZs) ./ sZs
-# get least squares fit using only the training data
-x = [ones(size(Zs,1)) Zs]
-beta = x\thetas
-# least squares errors, both train and test, using coefficients only from train
-preprocess = x*beta
-errors = thetas - preprocess
-# standardize the inputs using stds of training data
-sErrors = std(errors,1)
-errors = errors ./ sErrors
-Y = errors[1:trainsize,:]'
-X = Zs[1:trainsize,:]'
-YT = errors[trainsize+1:end,:]'
-XT = Zs[trainsize+1:end,:]'
-x = 0
-thetas = 0
-traintest = 0
-Zs = 0
-# prepare the montecarlo data the same way
+YT = Y[:,trainsize+1:end]
+XT = X[:,trainsize+1:end]
+Y = Y[:,1:trainsize]
+X = X[:,1:trainsize]
 montecarlo = load("dsge.jld", "montecarlo")
-YMC = montecarlo[:,1:9]'
-mcZs = montecarlo[:,10:end]
-XMC = (mcZs .- mZs) ./ sZs
-preprocess = [ones(size(mcZs,1),1) XMC]  * beta
-XMC = XMC'
+YMC, XMC, Y_orig = Preprocess(montecarlo)
 
