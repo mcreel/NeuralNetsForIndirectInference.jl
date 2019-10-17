bed423926519aa9d5eeaac3b8b23fd175519df7d
Michael Creel <michael.creel@uab.es>
2019-10-17 19:04:50 +0200

    working on new SV example

--- SV/MakeData.jl
+++ SV/MakeData.jl
@@ -1,20 +1,23 @@
 using Pkg
 Pkg.activate(".")
-using SV, Econometrics
+using SV, Econometrics, StatsBase
 using BSON: @save
 
 function MakeData()
     n = 1000
-    burnin = 100
-    S = Int(5e2)
+    burnin = 1000
+    S = Int(5e5) # size of training and testing
+    SS = 1000 # size of design 
     # true parameters
     α = exp(-0.736/2.0)
     ρ = 0.9
     σ = 0.363
     θtrue = [α, ρ, σ] # true param values, on param space
     lb = [0.0, 0.0, 0.0]
-    ub = [2.0, 0.99, 2.0]
+    ub = [1.0, 0.99, 1.0]
     data = 0.0
+    datadesign = 0.0
+    # training and testing
     for s = 1:S
         shocks_u = randn(n+burnin)
         shocks_e = randn(n+burnin)
@@ -26,12 +29,28 @@ function MakeData()
         end
         data[s,:] = vcat(θ, m)
     end
+    # design
+    for s = 1:SS
+        shocks_u = randn(n+burnin)
+        shocks_e = randn(n+burnin)
+        y = SVmodel(θtrue, n, shocks_u, shocks_e, false)
+        m = sqrt(n)*aux_stat(y)
+        if s == 1
+            datadesign = zeros(SS, size(vcat(θtrue, m),1))
+        end
+        datadesign[s,:] = vcat(θtrue, m)
+    end
+    # stack them to get quantiles
+    data = [data; datadesign]
     # for the aux. stats., use their quantiles instead of raw values
     for i = 4:size(data,2)
-        tmp = sortperm(data[:,i])
-        data[:,i] = tmp/S
+        tmp = data[:,i]
+        data[:,i] = denserank(tmp)/(S+SS)
     end
-    @save "data.bson" data
+    # unstack
+    datadesign = data[(S+1):end,:]
+    data = data[1:S,:]
+    @save "data.bson" data datadesign
     return nothing
 end
 MakeData()
--- SV/Train.jl
+++ SV/Train.jl
@@ -7,30 +7,34 @@ include("NNlib.jl")
 
 function main()
 whichdep = 2:2
-@load "data.bson" data
+@load "data.bson" data datadesign
 S = size(data,1)
 trainsize = Int(0.5*S)
 yin = data[1:trainsize, whichdep]'
 yout = data[trainsize+1:end, whichdep]'
 xin = data[1:trainsize, 4:end]'
 xout = data[trainsize+1:end, 4:end]'
-
+ydesign = datadesign[:, whichdep]'
+xdesign = datadesign[:, 4:end]'
+# model
 model = Chain(
-  Dense(size(xin,1),5,tanh),
-  Dense(5,5,tanh),
-  Dense(5,1)
+  Dense(size(xin,1),20,tanh),
+  Dense(20,20,tanh),
+  Dense(20,20,tanh),
+  Dense(20,3)
 )
 θ = Flux.params(model)
 opt = AdaMax()
 #loss(x,y) = Flux.mse(model(x),y) # + 0.1*L2penalty(θ) # QR with regularization
-loss(x,y) = SmoothQRobj(model(x),y, x, 0.95) # + 0.1*L2penalty(θ) # QR with regularization
+#loss(x,y) = SmoothQRobj(model(x),y, x, [0.05, 0.5, 0.95]) # + 0.1*L2penalty(θ) # QR with regularization
+loss(x,y) = QRobj(model(x),y, [0.05, 0.5, 0.95]) # + 0.1*L2penalty(θ) # QR with regularization
 function monitor(e)
     println("epoch $(lpad(e, 4)): (training) loss = $(round(loss(xin,yin).data; digits=4)) (testing) loss = $(round(loss(xout,yout).data; digits=4))| ")
 end
 bestsofar = 1.0e10
 inbatch = 0
 a = 0.0
-for i = 1:500
+for i = 1:5000
     inbatch = rand(size(xin,2)) .< 64.0/size(xin,2)
     batch = DataIterator(xin[:,inbatch],yin[:,inbatch])
     Flux.train!(loss, θ, batch, opt)
@@ -46,16 +50,16 @@ for i = 1:500
         elseif whichdep == 3:3    
             @save "best3.bson" model
         end    
-    end   
-    if i % 1 == 0
-        xx = xout
-        yy = yout
+    #end   
+    #if i % 1 == 0
+        xx = xdesign
+        yy = ydesign
         monitor(i)
         pred = (model(xx).data)
         results = yy .<= pred
         accuracy = mean(results,dims=2)
         println("% below quantile: ")
-        prettyprint(reshape(round.(accuracy,digits=3),1,1))
+        prettyprint(reshape(round.(accuracy,digits=3),1,3))
     end
 end
 end 
